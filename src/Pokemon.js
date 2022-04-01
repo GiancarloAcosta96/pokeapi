f25c6b4543e8b3397ffba0a484b958ef48cd3aa3
Gian <35286472+GiancarloAcosta96@users.noreply.github.com>
2022-04-01 09:14:39 -0500

    se añadió experiencia de pokemon

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -12,7 +12,8 @@ const Pokemon = (props) => {
             </div>
             <div>
                 <div>
-                    <h2 className="font-semibold text-center capitalize text-2xl">{pokemon.name}</h2>
+                    <h2 className="font-semibold  text-center capitalize text-2xl">{pokemon.name}</h2>
+                    <h2 className="text-2xl font-semibold text-center rounded-full bg-green-500">Exp: {pokemon.base_experience}</h2>
                     <h2 className="text-center text-xl">Tipo: {pokemon.types[0].type.name}</h2>
                     <h2 className="text-center text-xl">Peso: {pokemon.weight} kg.</h2>
                     <h2 className="text-center text-xl">Altura: {pokemon.height}m</h2>
